4f5db4183cef4b0800dc512a52d97030e0ccefea
Tim Lacey <tim.a.lacey@gmail.com>
2025-07-09 11:33:03 +1000

    input field for P, track via React.useState

--- App.jsx
+++ App.jsx
@@ -1,10 +1,47 @@
+import React from 'react';
 import calculateTermDepositReturn from './src/calculateTermDepositReturn.js'
 
+const INPUTS = {
+  "P": { defaultValue: 10000, min: 1, max: 10 ** 20 },
+  "r": { defaultValue: 1.1, min: 0, max: 15 },
+  "n": { defaultValue: 0, min: 0, max: 12 },
+  "t": { defaultValue: 36, min: 0, max: 720 },
+};
+
 export default function App() {
+  const [state, setState] = React.useState({
+    P: INPUTS.P.defaultValue,
+  })
+
+  function onFieldChange(event) {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  const finalBalance = calculateTermDepositReturn(state.P, INPUTS.r.defaultValue, INPUTS.n.defaultValue, INPUTS.t.defaultValue);
+
   return (
     <>
-      <h1>Tim Lacey's Term Deposit Calculator</h1>
-      <p>Hello. {calculateTermDepositReturn(1000, 5, 4, 36)}</p>
+      <h1>Tim Lacey's Term Deposit Calculator for Ferocia</h1>
+      <form onChange={onFieldChange} >
+        <label>$</label>
+        <input
+          defaultValue={state.P}
+          name="P"
+          type="number"
+        />
+        <label>
+          in a Term Depsosit at {INPUTS.r.defaultValue}% paid {INPUTS.n.defaultValue} times per year for {INPUTS.t.defaultValue} months
+        </label>
+        <br />
+        <br />
+      </form>
+      {(finalBalance > 0) && <label>
+        will return {finalBalance.toLocaleString('en-AU', { style: 'currency', currency: 'AUD' })}
+      </label>
+      }
     </>
   )
 }
